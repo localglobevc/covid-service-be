f81b810eb69a2f1e7fd301d96e6222c08f7a429c
Louis Knight-Webb <stunningpixels@users.noreply.github.com>
2020-03-15 01:21:56 +0000

    Updated geocode

--- api/geocode/controllers/Geocode.js
+++ api/geocode/controllers/Geocode.js
@@ -4,22 +4,26 @@ const Client = require('@googlemaps/google-maps-services-js').Client;
 
 module.exports = {
   geocode: async (ctx) => {
-    const gclient = new Client({});
-    const GOOGLE_KEY = 'AIzaSyCKWPdrtSrSefYwm9rNRzBuBWhseZTg7VU';
+    try {
+      const gclient = new Client({});
+      const GOOGLE_KEY = 'AIzaSyCKWPdrtSrSefYwm9rNRzBuBWhseZTg7VU';
 
-    const geocode = await gclient.geocode({params:{address: ctx.params.postcode, key: GOOGLE_KEY}});
+      const geocode = await gclient.geocode({params:{address: ctx.params.postcode, key: GOOGLE_KEY}});
 
-    const {
-      short_name: neighbourhood
-    } = geocode.data.results[0].address_components.find(result => result.types[0] === 'neighborhood');
+      const {
+        short_name: neighbourhood
+      } = geocode.data.results[0].address_components.find(result => result.types[0] === 'neighborhood');
 
-    const {
-      short_name: postcode
-    } = geocode.data.results[0].address_components.find(result => result.types[0] === 'postal_code');
+      const {
+        short_name: postcode
+      } = geocode.data.results[0].address_components.find(result => result.types[0] === 'postal_code');
 
-    ctx.body = {
-      neighbourhood,
-      postcode
-    };
+      ctx.body = {
+        neighbourhood,
+        postcode
+      };
+    } catch (e) {
+      ctx.body = {error: 'Postcode not found'};
+    }
   }
 };
